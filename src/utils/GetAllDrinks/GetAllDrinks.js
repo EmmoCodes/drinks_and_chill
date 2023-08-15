b777d84e3b43f2dff38616b1a7216d3967b736cf
S4nt1nO <Gianluca1996@gmx.de>
2023-08-15 16:37:53 +0200

    fetch fix

--- src/utils/GetAllDrinks/GetAllDrinks.js
+++ src/utils/GetAllDrinks/GetAllDrinks.js
@@ -1,9 +1,10 @@
 export const getAllDrinks = (apiLink, setState) => {
   fetch(apiLink)
     .then(response => {
-      if (!response.ok) {
-        throw new Error('Something went wrong with fetch')
-      }
+      // if (!response.ok) {
+      //   throw new Error('Something went wrong with fetch')
+      // }
+      console.log(response)
       return response.json()
     })
     .then(response => setState(response.drinks))
