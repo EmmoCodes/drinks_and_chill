e8a1db29dac064237022c2bec685cc96856b1e3c
KSchlendrian <cbii@hotmail.de>
2023-08-16 11:39:04 +0200

    drinksitem styling

--- src/components/DrinksItems/DrinksItem.jsx
+++ src/components/DrinksItems/DrinksItem.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './DrinksItem.scss'
 
 function DrinksItem({ drink }) {
   return (
-    <div>
+    <div className="drinks_item">
       <img className="drink_list_img" src={drink.strDrinkThumb} alt="" />
       <h2 className="drink_list_name">{drink.strDrink}</h2>
     </div>
